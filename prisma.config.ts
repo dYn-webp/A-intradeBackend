51ca705839eea81490fc9dfeacdbe14cb05d94d7
dYn-webp <adindaardiansyah1135@gmail.com>
2026-05-13 22:41:18 +0700

    fix bug env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,8 @@
-// prisma.config.ts
-import { defineConfig, env } from 'prisma/config';
 import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  // PERBAIKAN: Schema harus berupa string langsung, bukan objek!
   schema: 'prisma/schema.prisma',
-  
-  // PERBAIKAN: Menggunakan 'datasource', bukan 'migrate'
   datasource: {
     url: env('DATABASE_URL'),
   },
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as dotenv from 'dotenv';
+
+// Pastikan file .env terbaca
+dotenv.config();
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // Tanpa constructor, biarkan Prisma membaca .env secara otomatis!
+  constructor() {
+    // Di Prisma 7, kita WAJIB menggunakan Driver Adapter
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    
+    super({ adapter } as any);
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
