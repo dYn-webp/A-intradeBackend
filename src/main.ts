f8c6c061a7ea74f65b4bf7af2298ce7c3c0a5261
dYn-webp <adindaardiansyah1135@gmail.com>
2026-05-13 14:49:41 +0700

    eror bk

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // PERBAIKAN: Aktifkan CORS agar Frontend bisa akses
+  app.enableCors({
+    origin: 'http://localhost:3000', // Port frontend Anda
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3001); // Jalankan di port 3001
 }
 bootstrap();
